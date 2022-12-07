241f937adeb84e849e18d851322d3a73f380c8dd
dveresko <119889261+dveresko@users.noreply.github.com>
2022-12-07 11:37:09 +0000

    primBut new

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,16 +3,16 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '142px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
         background: rgba(28,28,28,0);
         border: 1px solid #3B82F6;
-        border-radius: 16px;
+        border-radius: 24px;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: #152842;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: linear-gradient(180deg, #DFEAF1 0%, #F9FAFC 100%);
         }
         &:active {
             background: #DFDFDF;
