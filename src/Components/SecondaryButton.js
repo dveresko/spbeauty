54e75ad45b982d6e61a5d0ff6e7ebb84cdfc6df6
dveresko <119889261+dveresko@users.noreply.github.com>
2022-12-07 11:13:09 +0000

    PrimButton SecButton

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,10 +3,11 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '142px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
+        background: rgba(28,28,28,0);
+        border: 1px solid #3B82F6;
+        border-radius: 16px;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: #152842;
