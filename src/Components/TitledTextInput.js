e565af3d8d73475151abe6f6819cdabf6d2d0543
dveresko <119889261+dveresko@users.noreply.github.com>
2022-12-08 13:06:18 +0000

    input padding

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -18,7 +18,7 @@ function TitledTextInput({title, def}) {
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Input className='p-2' defaultValue={def} ></Input>
+            <Input className='px-3 py-2' defaultValue={def} ></Input>
         </div>
     );
 }
