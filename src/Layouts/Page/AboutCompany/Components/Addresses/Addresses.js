070cee60eb3476978e66992a881f44bb087a2bab
dveresko <119889261+dveresko@users.noreply.github.com>
2022-12-16 07:38:46 +0000

    adresses gap

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,10 +4,8 @@ import ListElement from "./ListElement";
 function Addresses() {
     return (
         <div className='d-inline-flex justify-content-between flex-column  p-4 me-5 ms-5 col myborder'>
-            
-            <div className='title'>Адреса заведений</div>
-             
-            <div className='d-inline-flex flex-column gap-2'>
+            <div className='d-inline-flex flex-column  gap-4'>
+                <div className='title'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 {/* <div className='col'></div>  */}
